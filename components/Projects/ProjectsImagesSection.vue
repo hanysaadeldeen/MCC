efb6e10cca1e827b1feb297d5e37e18bbd303862
Hany Mohamed <hanymohamedsaadeldean@gmail.com>
2025-09-03 15:28:17 +0200

    Add Projects with Title

--- components/Projects/ProjectsImagesSection.vue
+++ components/Projects/ProjectsImagesSection.vue
@@ -2,96 +2,229 @@
     <section class="SuppliersBrandsSection">
         <div class="max-w-[1392px] mx-auto max-xl:px-6">
             <div class="flex max-lg:flex-col gap-2 md:gap-4 lg:gap-6">
-                <div class="w-full lg:w-1/2 imgContainer max-h-[584px]">
-                    <img src="~/assets/img/Projects/haramTower.webp" class="h-full max-h-full object-cover w-full "
-                        alt="haramTower">
+                <div class="relative w-full lg:w-1/2 imgContainer max-h-[584px] group">
+                    <img src="~/assets/img/Projects/King Abdulaziz L.webp" loading="lazy"
+                        class="h-full max-h-full object-cover w-full " alt="  مشروع مطار الملك
+                            عبد العزيز الدولي (KAIA)">
+                    <div
+                        class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                        <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                            مشروع مطار الملك
+                            عبد العزيز الدولي (KAIA)
+                        </h1>
+                    </div>
                 </div>
                 <div class="flex flex-col w-full lg:w-1/2 gap-2 md:gap-4 lg:gap-6">
-                    <div class="h-1/2 imgContainer bg-red-500">
-                        <img src="~/assets/img/Projects/Economic City.webp" class="w-full h-full object-cover"
-                            alt="Abraj Al Bait Towers - Day">
+                    <div class="h-1/2 imgContainer group relative">
+                        <img src="~/assets/img/Projects/Economic City.webp" loading="lazy"
+                            class="w-full h-full object-cover" alt="  مشروع مدينة الملك عبد الله الاقتصادية (KAEC)">
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع مدينة الملك عبد الله الاقتصادية (KAEC)
+                            </h1>
+                        </div>
+
                     </div>
                     <div class="flex gap-2 md:gap-4 lg:gap-6 h-1/2 ">
-                        <div class="imgContainer  w-1/2">
-                            <img src="~/assets/img/Projects/AMAALA L.webp" class="w-full h-full max-w-full object-cover"
-                                alt="Traditional Mud-Brick Village">
+                        <div class="imgContainer  w-1/2 group relative">
+                            <img src="~/assets/img/Projects/AMAALA L.webp" loading="lazy"
+                                class="w-full h-full max-w-full object-cover" alt="  مشروع أمالا">
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع أمالا
+                                </h1>
+                            </div>
+
                         </div>
-                        <div class="imgContainer  w-1/2">
+                        <div class="imgContainer  w-1/2 group relative">
                             <img src="~/assets/img/Projects/Salman Park L.webp"
-                                class="w-full h-full max-w-full object-cover" alt="Traditional Architecture">
+                                class="w-full h-full max-w-full object-cover" loading="lazy"
+                                alt=" مشروع حديقة الملك سلمان (KSP)">
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع حديقة الملك سلمان (KSP)
+                                </h1>
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
             <div class="flex max-lg:flex-col gap-2 md:gap-4 lg:gap-6 flex-row-reverse mt-2 md:mt-4 lg:mt-6 ">
-                <div class="w-full lg:w-1/2 imgContainer max-h-[280px]">
-                    <img src="~/assets/img/Projects/NEOM.webp" class="object-cover w-full h-full" alt="NEOM">
+                <div class="w-full lg:w-1/2 imgContainer max-h-[280px] group relative">
+                    <img src="~/assets/img/Projects/NEOM.webp" class="object-cover w-full h-full" loading="lazy"
+                        alt="  مشروع نيوم (أوكساجون، سندالة، تروجينا)">
+
+                    <div
+                        class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                        <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                            مشروع نيوم (أوكساجون، سندالة، تروجينا)
+                        </h1>
+                    </div>
                 </div>
                 <div class="flex gap-2 md:gap-4 lg:gap-6 w-full lg:w-1/2 ">
-                    <div class="imgContainer  w-1/2">
+                    <div class="imgContainer  w-1/2 group relative">
                         <img src="~/assets/img/Projects/Red Sea L.webp" class="object-cover w-full h-full"
-                            alt="Red Sea ">
+                            loading="lazy" alt="   مشروع البحر الأحمر">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع البحر الأحمر
+                            </h1>
+                        </div>
                     </div>
-                    <div class="imgContainer  w-1/2">
+                    <div class="imgContainer  w-1/2 group relative">
                         <img src="~/assets/img/Projects/King Saud L.webp" class="object-cover w-full h-full"
-                            alt="King Saud ">
+                            loading="lazy" alt=" مشروع جامعة الملك سعود (KSU)">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع جامعة الملك سعود (KSU)
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="flex max-lg:flex-col gap-2 md:gap-4 lg:gap-6 flex-row-reverse mt-2 md:mt-4 lg:mt-6">
-                <div class="w-full lg:w-1/2 imgContainer">
+                <div class="w-full lg:w-1/2 imgContainer group relative">
                     <img src="~/assets/img/Projects/University of Science L.webp"
-                        class="object-cover w-full max-w-full max-h-full block" alt="University of Science ">
+                        class="object-cover w-full max-w-full max-h-full block" loading="lazy"
+                        alt="  مشروع جامعة الملك عبد الله للعلوم والتقنية (KAUST)">
+
+                    <div
+                        class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                        <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                            مشروع جامعة الملك عبد الله للعلوم والتقنية (KAUST)
+                        </h1>
+                    </div>
                 </div>
                 <div class="flex flex-col w-full lg:w-1/2 gap-2 md:gap-4 lg:gap-6">
-                    <div class="h-1/2 imgContainer ">
-                        <img src="~/assets/img/Projects/Jabal Omar.webp" class="w-full object-cover"
-                            alt="Abraj Al Bait Towers - Day">
+                    <div class="h-1/2 imgContainer group relative ">
+                        <img src="~/assets/img/Projects/Jabal Omar.webp" class="w-full object-cover" loading="lazy"
+                            alt="       مشروع جبل عمر للتطوير (JODP)">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع جبل عمر للتطوير (JODP)
+                            </h1>
+                        </div>
                     </div>
                     <div class="flex gap-2 md:gap-4 lg:gap-6 h-1/2 ">
-                        <div class="imgContainer  w-1/2">
+                        <div class="imgContainer  w-1/2 group relative">
                             <img src="~/assets/img/Projects/ALULA L.webp" class="w-full max-w-full object-cover"
-                                alt="ALULA L">
+                                loading="lazy" alt=" مشروع العلا">
+
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع العلا
+                                </h1>
+                            </div>
                         </div>
-                        <div class="imgContainer  w-1/2">
+                        <div class="imgContainer  w-1/2 group relative">
                             <img src="~/assets/img/Projects/MASAAR L.webp" class="w-full max-w-full object-cover"
-                                alt="MASAAR ">
+                                loading="lazy" alt="مشروع مسار ">
+
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع مسار
+                                </h1>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="flex max-lg:flex-col gap-2 md:gap-4 lg:gap-6 flex-row-reverse mt-2 md:mt-4 lg:mt-6 ">
-                <div class="w-full lg:w-1/2 imgContainer max-h-[280px]">
+                <div class="w-full lg:w-1/2 imgContainer max-h-[280px] group relative">
                     <img src="~/assets/img/Projects/King Abdullah.webp" class="object-cover w-full h-full"
-                        alt="King Abdullah">
+                        loading="lazy" alt="   مشروع مركز الملك عبد الله المالي (KAFD)">
+
+                    <div
+                        class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                        <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                            مشروع مركز الملك عبد الله المالي (KAFD)
+                        </h1>
+                    </div>
                 </div>
                 <div class="flex gap-2 md:gap-4 lg:gap-6 w-full lg:w-1/2 ">
-                    <div class="imgContainer  w-1/2 max-h-[280px]">
+                    <div class="imgContainer  w-1/2 max-h-[280px] group relative">
                         <img src="~/assets/img/Projects/Qiddiyah L.webp" class="object-cover w-full h-full"
-                            alt="Qiddiyah">
+                            loading="lazy" alt=" مشروع القدية">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع القدية
+                            </h1>
+                        </div>
                     </div>
-                    <div class="imgContainer  w-1/2 max-h-[280px]">
-                        <img src="~/assets/img/Projects/Amazon.webp" class="object-cover w-full h-full" alt="Amazon">
+                    <div class="imgContainer  w-1/2 max-h-[280px] group relative">
+                        <img src="~/assets/img/Projects/Amazon.webp" class="object-cover w-full h-full" loading="lazy"
+                            alt="  مشروع أمازون">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع أمازون
+                            </h1>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="flex max-lg:flex-col gap-2 md:gap-4 lg:gap-6 flex-row-reverse mt-2 md:mt-4 lg:mt-6">
-                <div class="w-full lg:w-1/2 imgContainer">
+                <div class="w-full lg:w-1/2 imgContainer group relative">
                     <img src="~/assets/img/Projects/King Khalid L.webp"
-                        class="object-fill w-full max-w-full max-h-full block" alt="King Khalid">
+                        class="object-fill w-full max-w-full max-h-full block" loading="lazy"
+                        alt="مشروع مطار الملك خالد الدولي (KKIA)">
+
+                    <div
+                        class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                        <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                            مشروع مطار الملك خالد الدولي (KKIA)
+                        </h1>
+                    </div>
                 </div>
                 <div class="flex flex-col w-full lg:w-1/2 gap-2 md:gap-4 lg:gap-6">
-                    <div class="h-1/2 imgContainer ">
-                        <img src="~/assets/img/Projects/Endowment.webp" class="w-full object-cover" alt="Endowment">
+                    <div class="h-1/2 imgContainer  group relative">
+                        <img src="~/assets/img/Projects/Endowment.webp" class="w-full object-cover" loading="lazy"
+                            alt=" مشروع وقف الملك عبد العزيز">
+
+                        <div
+                            class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                            <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                مشروع وقف الملك عبد العزيز
+                            </h1>
+                        </div>
                     </div>
                     <div class="flex gap-2 md:gap-4 lg:gap-6 h-1/2 ">
-                        <div class="imgContainer  w-1/2">
+                        <div class="imgContainer  w-1/2 group relative">
                             <img src="~/assets/img/Projects/Diriyah Gate L.webp" class="w-full max-w-full object-cover"
-                                alt="Diriyah Gate ">
+                                loading="lazy" alt="  مشروع برج ساعة مكة المكرمة">
+
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-3 md:p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع برج ساعة مكة المكرمة
+                                </h1>
+                            </div>
                         </div>
-                        <div class="imgContainer  w-1/2">
+                        <div class="imgContainer  w-1/2 group relative">
                             <img src="~/assets/img/Projects/Makkah Clock L.webp" class="w-full max-w-full object-cover"
-                                alt="Makkah Clock ">
+                                loading="lazy" alt="مشروع بوابة الدرعية">
+
+                            <div
+                                class="absolute -bottom-24 group-hover:bottom-0 transition-all duration-300 ease-in-out left-0 w-full h-fit p-5 Projecttitle">
+                                <h1 class="text-primary text-base md:text-lg lg:text-xl font-bold text-center">
+                                    مشروع بوابة الدرعية
+                                </h1>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -118,6 +251,7 @@
 
 .imgContainer:hover img {
     transform: scale(1.15);
+    filter: brightness(80%);
 }
 
 @media (max-width:768px) {
@@ -126,5 +260,9 @@
     .imgContainer img {
         border-radius: 12px;
     }
+}
+
+.Projecttitle {
+    background-color: rgba(255, 255, 255, 0.85);
 }
 </style>
